d169eaa2370251d7f07b0bf54f9de6c2aa073e19
holgihe <holgihe@eh-herrgen.com>
2023-09-17 01:59:22 +0200

    hi there

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(int argc, char** argv) {
-  std::string msg("hello-world");
+  std::string msg("hello-world, from hh");
   msg.append(std::to_string(827494));
   std::cout << msg << std::endl;
 }
