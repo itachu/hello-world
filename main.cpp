306cc1f0adce2d6890d95ca348326d6871ef82cf
holgihe <holgihe@eh-herrgen.com>
2023-09-17 02:26:56 +0200

    commit to new-test branch. V1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(int argc, char** argv) {
-  std::string msg("hello-world, from hh");
+  std::string msg("hello-world, from hh, this is new-test branch now");
   msg.append(std::to_string(827494));
   std::cout << msg << std::endl;
 }
